ddc7157e13ad8edeee696f38e256b7d81378a23f
Cel <celinenico22@gmail.com>
2021-03-02 21:36:37 -0500

    commit tree e10508f12e216c3b5152cac318af0a534a4e3e5d

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram 
+    attr_accessor :words 
+    
+    def initialize(words)
+        @words = words 
+    end 
+
+    def match(array)
+        array.select {|word| @words.split("").sort == word.split("").sort}
+    end 
+end 
